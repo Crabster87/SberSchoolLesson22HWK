623f8e7f1135c879453363d957937b6d4110c7e0
Crabster87 <iar876@yandex.ru>
2021-10-22 14:12:05 +0300

    permission

--- app/src/main/java/crabster/rudakov/sberschoollesson22hwk/MainActivity.java
+++ app/src/main/java/crabster/rudakov/sberschoollesson22hwk/MainActivity.java
@@ -1,7 +1,9 @@
 package crabster.rudakov.sberschoollesson22hwk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.provider.Telephony;
@@ -12,20 +14,36 @@ public class MainActivity extends AppCompatActivity implements SmsReceiveListene
 
     private SmsBroadcastReceiver receiver;
 
+    /**
+     * Помимо стандартных действий происходит регистрация 'SmsBroadcastReceiver',
+     * а также производится запрос разрешения на чтение СМС-сообщений
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECEIVE_SMS},
+                1);
+
         registerReceiver();
     }
 
+    /**
+     * Помимо стандартных действий производится отмена регистрации
+     *'SmsBroadcastReceiver'
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver();
     }
 
+    /**
+     * Метод создаёт объект 'SmsBroadcastReceiver', IntentFilter к нему и
+     * осуществляет его регистрацию
+     */
     private void registerReceiver() {
         receiver = new SmsBroadcastReceiver(this);
         IntentFilter intentFilter = new IntentFilter();
@@ -35,11 +53,17 @@ public class MainActivity extends AppCompatActivity implements SmsReceiveListene
         Log.d("RECEIVER", "WAS REGISTERED RECEIVER: " + receiver.getClass());
     }
 
+    /**
+     * Метод отменяет регистрацию 'SmsBroadcastReceiver'
+     */
     private void unregisterReceiver() {
         unregisterReceiver(receiver);
         receiver = null;
     }
 
+    /**
+     * Метод устанавливает полученный из СМС-сообщения код в TextView
+     */
     @Override
     public void onReceivedCode(String code) {
         ((TextView) findViewById(R.id.text_view)).setText(code);
--- app/src/main/java/crabster/rudakov/sberschoollesson22hwk/SmsBroadcastReceiver.java
+++ app/src/main/java/crabster/rudakov/sberschoollesson22hwk/SmsBroadcastReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
 
@@ -17,17 +18,32 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
         this.smsReceiveListener = new WeakReference<>(smsReceiveListener);
     }
 
+    /**
+     * В случае соответствия интента условиям интент-фильтра метод получает
+     * из него входящее СМС-сообщение, и преобразует его в строку, которую
+     * затем передаёт в 'SmsReceiveListener' для установки в TextView, в
+     * случае возникновения исключения показывается Toast-сообщение
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
             String smsBody = getSmsBody(intent);
             SmsReceiveListener listener = smsReceiveListener.get();
             if (listener != null)
-                listener.onReceivedCode(extractCode(smsBody));
-            Log.d("MESSAGE RECEIVED", "WAS RECEIVED CODE: " + smsBody);
+                try {
+                    listener.onReceivedCode(extractCode(smsBody));
+                } catch (IndexOutOfBoundsException e) {
+                    listener.onReceivedCode(context.getString(R.string.code_text_view));
+                    Toast.makeText(context, "Неверный формат сообщения!", Toast.LENGTH_SHORT).show();
+                }
+            Log.d("MESSAGE RECEIVED", "WAS RECEIVED MESSAGE: \"" + smsBody + "\"");
         }
     }
 
+    /**
+     * Метод извлекает из интента массив, состоящий из кусков полученного
+     * СМС-сообщения, и преобразует его в строку
+     */
     private String getSmsBody(Intent intent) {
         SmsMessage[] messageChunks = Telephony.Sms.Intents.getMessagesFromIntent(intent);
         StringBuilder smsBody = new StringBuilder();
@@ -37,7 +53,11 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
         return smsBody.toString();
     }
 
-    private String extractCode(String smsBody) {
+    /**
+     * Метод извлекает код из тела полученного СМС-сообщения определённого формата,
+     * выбрасывая исключение в случае, если какой-либо из индексов равен -1
+     */
+    private String extractCode(String smsBody) throws IndexOutOfBoundsException {
         int firstDigitIndex = smsBody.indexOf(":") + 2;
         int endPointIndex = smsBody.indexOf(".", firstDigitIndex);
         return smsBody.subSequence(firstDigitIndex, endPointIndex).toString();
